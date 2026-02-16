e6e8cb87510f3de54c485a77c445c90cac6d9ca8
TheArchons <github@thearchons.xyz>
2026-02-16 14:55:09 -0500

    add support for Opus 4.5 and 4.6

--- llm_bedrock_anthropic.py
+++ llm_bedrock_anthropic.py
@@ -258,6 +258,46 @@ def register_models(register):
             "bh-v4.5-global",
         ),
     )
+    register(
+        BedrockClaude(
+            "us.anthropic.claude-opus-4-5-20251101-v1:0", supports_attachments=True
+        ),
+        aliases=(
+            "bedrock-claude-v4.5-opus-us",
+            "bedrock-claude-opus-v4.5-us",
+            "bo-v4.5-us",
+        ),
+    )
+    register(
+        BedrockClaude(
+            "us.anthropic.claude-opus-4-6-v1", supports_attachments=True
+        ),
+        aliases=(
+            "bedrock-claude-v4.6-opus-us",
+            "bedrock-claude-opus-v4.6-us",
+            "bo-v4.6-us",
+        ),
+    )
+    register(
+        BedrockClaude(
+            "global.anthropic.claude-opus-4-5-20251101-v1:0", supports_attachments=True
+        ),
+        aliases=(
+            "bedrock-claude-v4.5-opus-global",
+            "bedrock-claude-opus-v4.5-global",
+            "bo-v4.5-global",
+        ),
+    )
+    register(
+        BedrockClaude(
+            "global.anthropic.claude-opus-4-6-v1", supports_attachments=True
+        ),
+        aliases=(
+            "bedrock-claude-v4.6-opus-global",
+            "bedrock-claude-opus-v4.6-global",
+            "bo-v4.6-global",
+        ),
+    )
 
 
 class BedrockClaude(llm.Model):
